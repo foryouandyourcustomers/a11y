6ea149e581c9150160528dbc43f4b1a9faa1b1b3
Matias Meno <m@tias.me>
2023-10-18 20:27:21 +0200

    Add entries for all individual snapshots of a website

---
+++ web/src/routes/monitor/[website]/[snapshot]/+page.server.ts
@@ -0,0 +1,20 @@
+import { loadAllWebsites, loadWebsiteSnapshots } from '$lib/utils'
+
+export const entries = async () => {
+	const websites = await loadAllWebsites()
+
+	const entries = []
+
+	for (const website of websites) {
+		const snapshots = await loadWebsiteSnapshots(website.name)
+
+		for (const snapshot of snapshots) {
+			entries.push({
+				website: website.name,
+				snapshot: snapshot.date
+			})
+		}
+	}
+
+	return entries
+}
