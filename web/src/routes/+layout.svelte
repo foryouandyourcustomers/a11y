cd9255e574ad2cbbfae8deb12ec95b1877906e03
Matias Meno <m@tias.me>
2023-10-18 21:17:27 +0200

    Add base

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 	import '$lib/style/app.css'
 	import Header from '$lib/components/Header.svelte'
+
+	import { base } from '$app/paths'
 </script>
 
+<svelte:head>
+	{#if base}
+		<base href="https://foryouandyourcustomers.github.io{base}" />
+	{/if}
+</svelte:head>
+
 <Header />
+
 <main>
 	<slot />
 </main>
